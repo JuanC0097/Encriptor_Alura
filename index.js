757e0425944b0e1c51f9cab2cad07352fa732918
JuanC0097 <juanvillarq0097@gmail.com>
2023-01-14 14:57:16 -0500

    Modificación de la funcion imprimir texto, Creación de la funcion para reemplazar acentos, Creación de la función para extraer caracteres especiales,Creacion de documentación para el codigo actual

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-//Guardar en constantes los input
-const txtEncryptor = document.querySelector("#encryptor_text");
+//constantes con los input
+const txtEncryptor = document.getElementById("encryptor_text");
 const btnEncryptor = document.querySelector("#encriptor");
 const btnDencryptor = document.querySelector("#dencryptor");
 const presult = document.querySelector("#result");
+const accents = document.querySelector("#acents");
 
-//agregar el evento al boton encriptar 
-btnEncryptor.addEventListener("click", prinText)
+//Evento del boton encriptar 
+btnEncryptor.addEventListener("click", prinText);
 
 
-//funcion para mostrar el texto
-function prinText() {
 
-    texto = txtEncryptor.value;
-    presult.innerText = "SU TEXTO ES: " + texto;
+//FUNCIONES
+
+/* Funcion Principal:
+   - Guarda el valor del input en una variable
+   - Con la funcion removeAccents. Reemplazara las vocales con tilde,por vocales sin tildes
+   - Con la funcion removeSpecialChar. Extraera los caracteres especiales del string*/
+function prinText() { 
+
+    let texto = txtEncryptor.value;
+    removetxt = removeAccents(texto);
+    removetxt = removeSpecialChar(removetxt);
+    presult.innerText = "SU TEXTO ES: " + removetxt;
 
 }
+
+//Constante para probar las funciones
+const textoPrueba = "esté es ún textó de pruebá í";
+
+//Diccionario para reemplazar los acentos.
+let diccionario =
+{
+  "á" : "a",
+  "é" : "e",
+  "í" : "i",
+  "ó" : "o",
+  "ú" : "u"
+};
+
+//Función para remover acentos
+function removeAccents (frase) {
+
+    remove = frase.replace(/á|é|í|ó|ú/g,function(a){
+      return diccionario[a];
+    });
+    return remove;
+
+}
+
+//Función para remover caracteres especiales.
+function removeSpecialChar(frase) {
+
+    text = frase.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+    return text;
+
+}
+
